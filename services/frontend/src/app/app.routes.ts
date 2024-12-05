ddf024e7082a500ef7e01f38ee91458fd1122811
Muhamad Aji Wibisono <muhamadajiw71@gmail.com>
2024-12-06 00:38:47 +0700

    feat: jargon header

--- services/frontend/src/app/app.routes.ts
+++ services/frontend/src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from '@app/about/about.component';
 
-export const routes: Routes = [{ path: '', component: AboutComponent }];
+export const routes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'about', component: AboutComponent },
+];
