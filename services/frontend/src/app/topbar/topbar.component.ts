1045fb9a3856febcaac0a4bb4a354412bfc81461
Muhamad Aji Wibisono <muhamadajiw71@gmail.com>
2024-12-09 21:27:55 +0700

    feat: dynamic topbar

--- services/frontend/src/app/topbar/topbar.component.ts
+++ services/frontend/src/app/topbar/topbar.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-topbar',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, CommonModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss',
 })
-export class TopbarComponent {}
+export class TopbarComponent implements OnInit {
+  @Input() fixed: boolean = false;
+  @Input() limit: number = 400;
+  private debounceTimeout: any;
+  isScrolled = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    clearTimeout(this.debounceTimeout);
+    this.debounceTimeout = setTimeout(() => {
+      this.checkScroll();
+    }, 100);
+  }
+
+  checkScroll() {
+    this.isScrolled =
+      this.fixed || window.scrollY > this.limit || window.scrollY < 5;
+  }
+}
