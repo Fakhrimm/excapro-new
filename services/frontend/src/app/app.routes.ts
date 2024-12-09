5f7318692f00d15872a03b52f3c8ea353c2a8284
Muhamad Aji Wibisono <muhamadajiw71@gmail.com>
2024-12-09 23:57:44 +0700

    feat: aggregated pages for dashboard

--- services/frontend/src/app/app.routes.ts
+++ services/frontend/src/app/app.routes.ts
@@ -1,20 +1,13 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from '@app/pages/about/about.component';
-import { UsersComponent } from './pages/users/users.component';
-import { SettingsComponent } from './pages/settings/settings.component';
-import { EventsComponent } from './pages/events/events.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
-import { StatisticsComponent } from './pages/statistics/statistics.component';
 
 export const routes: Routes = [
   { path: '', component: AboutComponent },
   { path: 'about', component: AboutComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'events', component: EventsComponent },
-  { path: 'settings', component: SettingsComponent },
-  { path: 'stats', component: StatisticsComponent },
-  { path: 'users', component: UsersComponent },
-
+  { path: 'dashboard/:view', component: DashboardComponent },
+  { path: 'auth', component: LoginComponent },
   { path: 'auth/:type', component: LoginComponent },
 ];
--- services/frontend/src/app/pages/dashboard/dashboard.component.ts
+++ services/frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { EventsComponent } from './events/events.component';
+import { SettingsComponent } from './settings/settings.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    CommonModule,
+    EventsComponent,
+    SettingsComponent,
+    StatisticsComponent,
+    UsersComponent,
+    HomeComponent,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  view: string = '';
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.view = params.get('view')!;
+    });
+  }
 }
